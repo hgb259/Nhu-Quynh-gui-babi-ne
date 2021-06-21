e36b7a99111928806e957bc29fb4da16f2ddc6c5
hgb259 <85253996+hgb259@users.noreply.github.com>
2021-06-22 00:41:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'ylnautiamel iu à!',
+    introTitle: 'Hạnh Trang à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao anh cứ đi nhầm đường
+    Mà sao chị cứ đi nhầm đường
     Lạc vào tim em lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích em <33',
-    btnNo: 'Không, em gái à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em <3',
+    btnNo: 'Không, em trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu chị <3',
     btnReply: 'Gửi cho em <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Em biết mà 🥰. Yêu anh nhiều nhiều 😘😘',
-    messDesc: 'Hết dịch em về PT với anh.',
+    mess: 'Em biết mà 🥰. Yêu chị nhiều nhiều 😘😘',
+    messDesc: 'Mai 7r nhé chị yêu của em.',
     btnAccept: 'Okiiiii lun <3',
     
 }
